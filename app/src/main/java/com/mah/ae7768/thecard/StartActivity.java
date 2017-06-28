02d448e51d4de5820638921a136106528ca709f8
AlexanderSiech <alex.siech@gmail.com>
2017-06-28 14:33:31 +0200

    Initial commit

---
+++ app/src/main/java/com/mah/ae7768/thecard/StartActivity.java
@@ -0,0 +1,20 @@
+package com.mah.ae7768.thecard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alexander on 2017-06-28.
+ */
+
+public class StartActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/java/com/mah/ae7768/thecard/User.java
@@ -0,0 +1,8 @@
+package com.mah.ae7768.thecard;
+
+/**
+ * Created by alexander on 2017-06-28.
+ */
+
+public class User {
+}
