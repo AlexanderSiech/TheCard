eda4c109796241e1595d65f5d14d96d0bdef000a
Girondins <stmsfsd@gmail.com>
2017-06-29 20:24:58 +0200

    StartUserDB

--- app/src/main/java/com/finkevolution/thecard/ExternalServices/UserDB.java
+++ app/src/main/java/com/finkevolution/thecard/ExternalServices/UserDB.java
@@ -1,8 +1,39 @@
 package com.finkevolution.thecard.ExternalServices;
 
 /**
- * Created by Girondins on 2017-06-28.
+ * Created by FinkEvolution on 2017-06-28.
  */
+import com.mongodb.MongoClient;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
 
 public class UserDB {
+    MongoClient mongoClient;
+    MongoDatabase database;
+    MongoCollection<Document> usersCollection;
+
+
+    public UserDB(){
+        mongoClient = new MongoClient( "localhost" , 27017 );
+        database = mongoClient.getDatabase("test");
+        usersCollection = database.getCollection("users");
+        Document doc = new Document("user_identifier","Mohee").append("Wallah","Tack");
+        usersCollection.insertOne(doc);
+    }
+
+    public void test(){
+        MongoCursor<Document> cursor = usersCollection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
 }
